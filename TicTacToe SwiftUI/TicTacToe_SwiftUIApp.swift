6d6f9ef57e2a353abdfce453e0aa198dc81c22ad
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-21 12:32:28 +0100

    Initial Commit

---
+++ TicTacToe SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe SwiftUI
+//
+//  Created by Giorgio Giannotta on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
